ecf78aed421449e88711b3af6b9e1a4fe48ec340
Ryan Stocker <stocker.19@osu.edu>
2013-09-08 11:53:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TomatoBag::Application.config.secret_key_base = '2210b99ba2e466ba6ea756d4a6da72bef0796c25515c8a3ebbc9c30d769287a5bed47fdcc3bdd1b813177810b7a6601c53f8b4bd5ee82c7b95e3f9ac5304f952'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TomatoBag::Application.config.session_store :cookie_store, key: '_tomato_bag_session'
