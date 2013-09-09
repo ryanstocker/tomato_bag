8373e94fe0c6132ed3602d4e00f8dce874bbdf6b
Ryan Stocker <stocker.19@osu.edu>
2013-09-09 14:59:26 -0400

    override devise alert styles & shared notices partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # strong parameters are done at the controller level in Rails 4
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
   def rotten_tomatoes
     RottenTomatoes.new(ENV['ROTTEN_TOMATOES_API_KEY'])
   end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
