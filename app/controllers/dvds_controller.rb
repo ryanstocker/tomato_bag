5c7683c27e4067720d456b64f3af10d61318bc34
Ryan Stocker <stocker.19@osu.edu>
2013-09-10 17:23:14 -0400

    Fix upcoming_dvds

--- app/controllers/dvds_controller.rb
+++ app/controllers/dvds_controller.rb
@@ -1,11 +1,18 @@
 class DvdsController < ApplicationController
+  before_filter :load_wanted_movie_ids
+
   def new_releases
-    @wanted_movie_ids = []
-    @wanted_movie_ids = current_user.flagged_movies.wanted.pluck(:rt_movie_id) if user_signed_in?
     @movies = rotten_tomatoes.new_dvd_releases
   end
 
   def upcoming_releases
     @movies = rotten_tomatoes.upcoming_dvd_releases
   end
+
+    private
+
+    def load_wanted_movie_ids
+      @wanted_movie_ids = []
+      @wanted_movie_ids = current_user.flagged_movies.wanted.pluck(:rt_movie_id) if user_signed_in?
+    end
 end
