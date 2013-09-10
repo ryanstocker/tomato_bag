026bee7dcab534270125e13ede8151abff3a6066
Ryan Stocker <stocker.19@osu.edu>
2013-09-09 23:42:59 -0400

    friendly redirection controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,15 @@ class ApplicationController < ActionController::Base
     RottenTomatoes::Api.instance
   end
 
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
