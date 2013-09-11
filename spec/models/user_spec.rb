d83233c3352e980647cadec622f573b5b93f47cd
Ryan Stocker <stocker.19@osu.edu>
2013-09-10 23:28:47 -0400

    user spec coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user)  { create(:user) }
+  let(:movie) { Movie.new(mock_movie_attributes) }
+  let!(:flagged_movie) { create(:flagged_movie, user: user, rt_movie_id: movie.id, state: 'wanted') }
+
+  describe '.wanted_movie?' do
+    it 'should return true for a wanted movie' do
+      expect(user.wanted_movie?(movie)).to be_true
+    end
+
+    it 'should return false for a watched movie' do
+      flagged_movie.update_attributes(state: 'watched')
+      expect(user.wanted_movie?(movie)).to be_false
+    end
+  end
 end
